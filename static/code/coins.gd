c4e94857311b5c1c3f5e31e53fe6d578f0934b5b
jvansant13 <jermey.vansant@davinciacademy.org>
2025-03-20 20:22:19 -0600

    updated comments

--- static/code/coins.gd
+++ static/code/coins.gd
@@ -1,5 +1,8 @@
 extends Area2D
 class_name Coin
+## This Script can easily be modified for a variety of pickup items.
+## It looks to see if the character has a function called collect_coin,
+## if so it calls it then it deletes itself
 
 
 ## Sees if player has function to collect coin, if so it does
